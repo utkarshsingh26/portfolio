1e26e487713411da0ca54df533e5d9c362d847ae
Utkarsh Singh <singh.utkarsh26@gmail.com>
2024-09-17 16:53:48 -0700

    basic publication without react states

--- src/components/Publications/Publications.jsx
+++ src/components/Publications/Publications.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 
 import styles from "./Publications.module.css";
+import publications from "../../data/publications.json";
+import { getImageURL } from "../../utils";
 
 const Publications = () => {
-  return (
-    <section className={styles.container} id="projects">
-        <h2 className={styles.title}>Publications</h2>
-    </section>
-  )
+    console.log(publications)
+    return (
+        <section className={styles.container} id="projects">
+          <h2 className={styles.title}>Publications</h2>
+          <div>
+        {publications.map((publication, id) => (
+            <div className={styles.publication}>
+                <img className={styles.img}
+                  src={getImageURL(publication.imageSrc)}
+                  alt={`${publication.publisher} Logo`}
+                />
+                <h3 key={id}>
+                    <a href={publication.url} key={id} target='_blank'>{publication.title}</a>
+                </h3>
+
+            </div>
+        ))}
+          </div>
+        </section>
+      );
 }
 
 export default Publications
